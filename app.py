afa57840bb39aa1608eb173a281b8cd012f3b8be
Zackdan0227 <kz2028@nyu.edu>
2022-10-18 17:57:05 -0400

    added content block to base template and login template

--- app.py
+++ app.py
@@ -16,25 +16,28 @@ config = dotenv_values(".env")
 # turn on debugging if in development mode
 if config['FLASK_ENV'] == 'development':
     # turn on debugging, if in development
-    app.debug = True # debug mnode
+    app.debug = True  # debug mnode
 
 
 # connect to the database
 cxn = pymongo.MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
 try:
     # verify the connection works by pinging the database
-    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
-    db = cxn[config['MONGO_DBNAME']] # store a reference to the database
-    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+    # The ping command is cheap and does not require auth.
+    cxn.admin.command('ping')
+    db = cxn[config['MONGO_DBNAME']]  # store a reference to the database
+    # if we get here, the connection worked!
+    print(' *', 'Connected to MongoDB!')
 except Exception as e:
     # the ping command failed, so the connection is not available.
     # render_template('error.html', error=e) # render the edit template
     print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
-    print('Database connection error:', e) # debug
+    print('Database connection error:', e)  # debug
+
 
 @app.route('/')
 def home():
     """
     Route for the home page
     """
-    return render_template('index.html') # render the hone template
+    return render_template('index.html')  # render the hone template
